a86ef3a2e5875595bfadbcd4f170a5c40650968e
Alex <awintschel@gmail.com>
2017-06-18 12:23:00 -0400

    completed title case

--- to-title-case.js
+++ to-title-case.js
@@ -1,23 +1,18 @@
-function toTitleCase(str) {
-  var firstLetters = [];
-  var caps = [];
-  var restOfWords = [];
-  var lowCase = [];
-  var titleCase = [];
-  var newSentence;
-  var myArr = str.split(" ");
-  for (i = 0; i < myArr.length; i++) {
-    firstLetters.push(myArr[i].slice(0,1));
-    restOfWords.push(myArr[i].slice(1));
-    lowCase[i] = restOfWords[i].toLowerCase();
-    caps[i] = firstLetters[i].toUpperCase();
-    titleCase[i] = caps[i].concat(lowCase[i]);
-    newSentence = titleCase.join(' ');
-  }
-  return newSentence;
-}
 
-toTitleCase("I'm a little tea pot");
-toTitleCase("ShiveR mE TIMBERS");
-toTitleCase("sHoRt AnD sToUt");
-toTitleCase('HERE IS MY HANDLE HERE IS MY SPOUT');
+  String.prototype.replaceAt = function(index, character) {
+      return this.substr(0, index) + character + this.substr(index+character.length);
+  };
+  function titleCase(str) {
+      var newTitle = str.split(' ');
+      var updatedTitle = [];
+      for (var st in newTitle) {
+          updatedTitle[st] = newTitle[st].toLowerCase().replaceAt(0, newTitle[st].charAt(0).toUpperCase());
+      }
+      return updatedTitle.join(' ');
+  }
+
+
+titleCase("I'm a little tea pot");
+titleCase("ShiveR mE TIMBERS");
+titleCase("sHoRt AnD sToUt");
+titleCase('HERE IS MY HANDLE HERE IS MY SPOUT');
