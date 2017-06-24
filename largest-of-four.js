4dafb0edcff764fa67f9a29732de8dedcebf3f78
Alex <awintschel@gmail.com>
2017-06-24 16:41:10 -0400

    finished largest-of-four

--- largest-of-four.js
+++ largest-of-four.js
@@ -1,6 +1,14 @@
-
 function largestOfFour(arr) {
-  
-  return arr;
+  var largestNums = arr.map(function(item) {
+    return item.sort(function(a, b) {
+      return a - b;
+    }).pop();
+  });
+  return largestNums;
 }
-largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+largestOfFour([
+  [4, 5, 1, 3, 51, 15, 109, 31],
+  [13, 27, 18, 26],
+  [32, 35, 37, 39],
+  [1000, 1001, 857, 1]
+]);
