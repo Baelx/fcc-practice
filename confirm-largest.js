2dbf6f52ff8c8718243f9c44a499bb47ad63f54a
Alex <awintschel@gmail.com>
2017-06-25 18:16:50 -0400

    finished confirm larget

---
+++ confirm-largest.js
@@ -0,0 +1,9 @@
+function confirmEnding(str, target) {
+  return str.substr(-target.length) === target) {
+}
+
+confirmEnding("Bastian", "n");
+
+
+/* Explanations and other methods:
+ https://forum.freecodecamp.com/t/freecodecamp-algorithm-challenge-guide-confirm-the-ending/16006/5 */
