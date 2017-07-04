9777a231a50cf39d1298ab37f6d42549672f36ba
Alex Wintschel <alex.wintschel@MacdeMarco.local>
2017-07-04 19:19:57 -0400

    made progress on mutation.js

--- mutation.js
+++ mutation.js
@@ -1,7 +1,13 @@
 function mutation(arr) {
-var letter = arr[0].charAt(3).toLowerCase();
-var secondWord
-console.log(letter);
+	for (var i = 0; i < arr[0].length;i++){
+		for (var x = 0; x < arr[1].length;x++){
+			// if (arr[1].charAt(x).toLowerCase().indexOf(arr[0].charAt(i).toLowerCase()) >= 0) 
+			console.log("Index of shows " + arr[1].charAt(x).toLowerCase().indexOf(arr[0].charAt(i).toLowerCase()));
+			console.log(arr[0].charAt(i).toLowerCase());
+			console.log(arr[0].charAt(x).toLowerCase());
+		}
+	}
+	// return true;
 }
 
-mutation(["helLo", "hey"]);
+mutation(["helLO", "hey"]);
