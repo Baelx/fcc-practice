b8973e2bbddb784f3e8a7acee0bb4864968f9839
Alex <awintschel@gmail.com>
2017-07-28 23:41:17 -0400

    finished mutation.js

--- mutation.js
+++ mutation.js
@@ -1,16 +1,11 @@
 function mutation(arr) {
-	for (var i = 0; i < arr[0].length;i++){
-		for (var x = 0; x < arr[1].length;x++){
-			// if (arr[1].charAt(x).toLowerCase().indexOf(arr[0].charAt(i).toLowerCase()) >= 0)
-			console.log("Index of shows " + arr[1].charAt(x).toLowerCase().indexOf(arr[0].charAt(i).toLowerCase()));
-			console.log(arr[0].charAt(i).toLowerCase());
-			console.log(arr[0].charAt(x).toLowerCase());
-		}
-	}
-	// return true;
-}
+  var test = arr[1].toLowerCase();
+  var target = arr[0].toLowerCase();
+  for (i=0;i<test.length;i++) {
+    if (target.indexOf(test[i]) === -1)
+      return false;
+  }
+  return true;
+ }
 
-mutation(["helLO", "hey"]);
-
-//At this point I need to stop the loops above if anything above -1 is outputted
-//I also need to make sure that if nothing above -1 is ouputted that it finishes looping to make sure
+  mutation(["sfCCKfUxUUHUp-oo", "fcc"]);
