4983a7d0a6083668c94c3da188121d1c56c569cc
Alex <awintschel@gmail.com>
2017-08-02 23:43:36 -0400

    updated latest function

--- seek-and-destroy.js
+++ seek-and-destroy.js
@@ -1,6 +1,10 @@
 function destroyer(arr) {
   // Remove all the values
-  return arr;
+var newArr = arr.filter(function(value){
+    console.log(value);
+    return value == false;
+  });
+  return newArr;
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);  
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
