e111af3924b4b1545f70f8909cc245cd02385a64
Alex <awintschel@gmail.com>
2017-08-10 23:00:22 -0400

    updated latest function

--- seek-and-destroy.js
+++ seek-and-destroy.js
@@ -1,8 +1,12 @@
 function destroyer(arr) {
   // Remove all the values
+  return arr.filter(function(n){
+  return n.typeof;
+})
 }
 
-destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
 
 //using filter and attributes object
 //filter will use function parameters as array values
+//
