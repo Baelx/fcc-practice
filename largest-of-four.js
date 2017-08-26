354c1eb72cd008c5015c50eb3716d382d346c784
Baelx <awintschel@gmail.com>
2017-08-26 08:01:17 -0400

    Update largest-of-four.js

--- largest-of-four.js
+++ largest-of-four.js
@@ -12,3 +12,5 @@ largestOfFour([
   [32, 35, 37, 39],
   [1000, 1001, 857, 1]
 ]);
+
+//Other solutions here: https://forum.freecodecamp.org/t/freecodecamp-algorithm-challenge-guide-return-largest-numbers-in-arrays/16042
