656ad5a43353537ebed06c8562a6582180ab03f8
Alex <awintschel@gmail.com>
2017-10-28 13:47:56 -0400

    completed seek and destroy

--- seek-and-destroy.js
+++ seek-and-destroy.js
@@ -1,12 +1,21 @@
+
 function destroyer(arr) {
-  // Remove all the values
-  return arr.filter(function(n){
-  return n.typeof;
-})
+
+  var args = Array.prototype.slice.call(arguments, 1);
+
+  var newArr = arguments[0].filter(function(arr){
+    return args.indexOf(arr) == -1;
+  });
+
+  return newArr;
 }
 
-console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+destroyer([1, 2, 3, 1, 2, 3], 2, 3, 5);
 
-//using filter and attributes object
-//filter will use function parameters as array values
-//
+/* Articles used:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments
+*/
+
+//Other solutions found here: https://forum.freecodecamp.org/t/freecodecamp-algorithm-challenge-guide-seek-and-destroy/16046
